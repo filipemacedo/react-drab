7fb215df846677eb807d68d566d8f0b879abb946
Filipe Macedo <devmacedo@gmail.com>
2019-11-23 19:22:01 -0300

    :hammer: Refactoring get paths of dirs

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -7,26 +7,32 @@ type ConfigType = {
 };
 
 const dir: string = process.cwd();
+const dirWithoutConfig = `${dir}/src`;
 
 export const config: ConfigType = require(`${dir}/drab.json`);
 
 /**
- *
+ * This function is responsible for get component path
  */
 export const getComponentDirPath: Function = (): string =>
-  `${dir}/${config.components}`;
+  config ? `${dir}/${config.components}` : `${dirWithoutConfig}/components`;
 
 /**
- *
+ * This function is responsible for get page path
  */
-export const getPageDirPath: Function = (): string => `${dir}/${config.pages}`;
+export const getPageDirPath: Function = (): string =>
+  config ? `${dir}/${config.pages}` : `${dirWithoutConfig}/pages`;
 
 /**
- *
+ * This function is responsible for get service path
  */
 export const getServiceDirPath: Function = (): string =>
-  `${dir}/${config.services}`;
+  config ? `${dir}/${config.services}` : `${dirWithoutConfig}/services`;
 
-export const getHookDirPath: Function = (): string => `${dir}/${config.hooks}`;
+/**
+ * This function is responsible for get hook path
+ */
+export const getHookDirPath: Function = (): string =>
+  config ? `${dir}/${config.hooks}` : `${dirWithoutConfig}/hooks`;
 
 export const getExtension: Function = (): string => config.extension || "js";
