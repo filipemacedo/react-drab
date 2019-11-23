c27db030e788bc1675f6de466a8df9904efd9a3a
Filipe Macedo <devmacedo@gmail.com>
2019-11-23 19:49:27 -0300

    :hammer: refactoring get config dir

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -14,31 +14,39 @@ const configPath = `${dir}/drab.json`;
 
 const configFile =
   fs.existsSync(configPath) && fs.readFileSync(`${dir}/drab.json`, "utf8");
-  
+
 export const config: ConfigType = configFile && JSON.parse(configFile);
 
 /**
  * This function is responsible for get component path
  */
 export const getComponentDirPath: Function = (): string =>
-  config ? `${dir}/${config.components}` : `${dirWithoutConfig}/components`;
+  config && config.components
+    ? `${dir}/${config.components}`
+    : `${dirWithoutConfig}/components`;
 
 /**
  * This function is responsible for get page path
  */
 export const getPageDirPath: Function = (): string =>
-  config ? `${dir}/${config.pages}` : `${dirWithoutConfig}/pages`;
+  config && config.pages
+    ? `${dir}/${config.pages}`
+    : `${dirWithoutConfig}/pages`;
 
 /**
  * This function is responsible for get service path
  */
 export const getServiceDirPath: Function = (): string =>
-  config ? `${dir}/${config.services}` : `${dirWithoutConfig}/services`;
+  config && config.services
+    ? `${dir}/${config.services}`
+    : `${dirWithoutConfig}/services`;
 
 /**
  * This function is responsible for get hook path
  */
 export const getHookDirPath: Function = (): string =>
-  config ? `${dir}/${config.hooks}` : `${dirWithoutConfig}/hooks`;
+  config && config.extension
+    ? `${dir}/${config.hooks}`
+    : `${dirWithoutConfig}/hooks`;
 
 export const getExtension: Function = (): string => config.extension || "js";
