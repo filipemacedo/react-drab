db907549ad0de8cd6d83cabae918f13043fafd35
Filipe Macedo <devmacedo@gmail.com>
2019-11-30 18:43:41 -0300

    Change var to const

--- clean.js
+++ clean.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 function removeBuildFolder(path) {
     if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
         fs.readdirSync(path).forEach(function(file) {
-            var curPath = path + "/" + file;
+            const curPath = path + "/" + file;
 
             if (fs.lstatSync(curPath).isDirectory()) {
                 removeBuildFolder(curPath);
